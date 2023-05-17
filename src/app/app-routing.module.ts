615aadeb6022c9cec92f3233a442b06f04575525
SteveAndr <sar.andrade@yavirac.edu.ec>
2023-05-17 18:53:51 +0200

    Tarea Modulos y Componentes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { NoFoundPageComponent } from './noFoundPage/no-found-page.component';
+
+import { PagesRoutingModule } from './pages/pages-routing.module';
+import { AuthComponent } from './auth/auth.component';
+const routes:Routes = [
+
+{path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+{path: 'auth', component: AuthComponent },
+{path: '**', component: NoFoundPageComponent} ,
+
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes),
+    PagesRoutingModule
+  ],
+  exports:[
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
+
+//ADRIAN MUÑOZ
